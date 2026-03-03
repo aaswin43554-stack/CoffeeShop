d37007122d1df2dd3a8118f8129d40a7ec29fcc3
aaswin43554-stack <aaswin43554@gmail.com>
2026-03-03 21:02:13 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["coffeeshop-1-ihqs.onrender.com"],
+    host: true
+  }
+});
